890ba5bc9320581d3fddd80a70f53ff35ec06f3a
ashleeeeeeyyyyy <puaphee@gmail.com>
2022-12-09 16:53:03 +0800

    Commit

---
+++ news-app/app/LiveTimestamp.tsx
@@ -0,0 +1,12 @@
+'use client';
+import TimeAgo from 'react-timeago';
+
+type Props = {
+  time: String;
+};
+
+function LiveTimestamp({ time }: Props) {
+  return <TimeAgo date={time} />;
+}
+
+export default LiveTimestamp;
